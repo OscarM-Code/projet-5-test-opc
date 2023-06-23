a6d1efecbed0afb1f870586bc51a29671e77f59d
Oscar <o.millot@kiwilab.fr>
2023-06-23 17:06:51 +0200

    ajout de test de services

--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -3,20 +3,49 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { TeacherService } from './teacher.service';
+import { of } from 'rxjs';
 
 describe('TeacherService', () => {
   let service: TeacherService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientModule],
     });
     service = TestBed.inject(TeacherService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should fetch all teachers', () => {
+    const mockTeachers = [
+      { id: '1', name: 'John Doe' },
+      { id: '2', name: 'Jane Smith' },
+      { id: '3', name: 'Michael Johnson' },
+    ];
+
+    // Mock the HTTP response for the 'all()' function
+    jest
+      .spyOn(service['httpClient'], 'get')
+      .mockReturnValueOnce(of(mockTeachers));
+
+    service.all().subscribe((teachers) => {
+      expect(teachers).toEqual(mockTeachers);
+    });
+  });
+
+  it('should fetch teacher detail by id', () => {
+    const mockTeacherId = '1';
+    const mockTeacher = { id: '1', name: 'John Doe' };
+
+    // Mock the HTTP response for the 'detail()' function
+    jest
+      .spyOn(service['httpClient'], 'get')
+      .mockReturnValueOnce(of(mockTeacher));
+
+    service.detail(mockTeacherId).subscribe((teacher) => {
+      expect(teacher).toEqual(mockTeacher);
+    });
   });
 });
--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,22 +1,48 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { UserService } from './user.service';
+import { of } from 'rxjs';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientModule],
     });
     service = TestBed.inject(UserService);
+    httpClient = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch user by id', () => {
+    const userId = '1';
+    const expectedUser = { id: '1', name: 'John Doe' };
+
+    // Mock the HTTP request for get() and return a predefined user
+    jest.spyOn(httpClient, 'get').mockReturnValue(of(expectedUser));
+
+    service.getById(userId).subscribe((user) => {
+      expect(user).toEqual(expectedUser);
+      expect(httpClient.get).toHaveBeenCalledWith('api/user/1'); // Verify the HTTP request was made with the correct URL
+    });
+  });
+
+  it('should delete user by id', () => {
+    const userId = '1';
+
+    // Mock the HTTP request for delete() and return an empty response
+    jest.spyOn(httpClient, 'delete').mockReturnValue(of(null));
+
+    service.delete(userId).subscribe((response) => {
+      expect(response).toBeNull();
+      expect(httpClient.delete).toHaveBeenCalledWith('api/user/1'); // Verify the HTTP request was made with the correct URL
+    });
   });
 });
