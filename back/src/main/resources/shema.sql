ce718a5c2b231b0ea26f575b7512482afe871899
Oscar <o.millot@kiwilab.fr>
2023-08-25 14:07:40 +0200

    test java sns erreur

--- back/src/main/resources/shema.sql
+++ back/src/main/resources/shema.sql
@@ -35,19 +35,3 @@ CREATE TABLE IF NOT EXISTS `PARTICIPATE` (
 ALTER TABLE `SESSIONS` ADD FOREIGN KEY (`teacher_id`) REFERENCES `TEACHERS` (`id`);
 ALTER TABLE `PARTICIPATE` ADD FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`);
 ALTER TABLE `PARTICIPATE` ADD FOREIGN KEY (`session_id`) REFERENCES `SESSIONS` (`id`);
-
-
-INSERT INTO TEACHERS (first_name, last_name)
-VALUES ('Margot', 'DELAHAYE'),
-       ('Hélène', 'THIERCELIN');
-
-
-INSERT INTO USERS (first_name, last_name, admin, email, password)
-VALUES ('Admin', 'Admin', true, 'yoga@studio.com', '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq'),
-       ('User', 'User', false, 'user@test.com', '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq');
-
-INSERT INTO `SESSIONS` (name, description, teacher_id, date)
-VALUES ('stretching', 'A stretching session', 1, '2023-01-01');
-
-INSERT INTO PARTICIPATE (user_id, session_id)
-VALUES (2, 1);
